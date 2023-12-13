5b1d669f51a8fb83cd1ef95b1d0fdbb77c92f01e
Satria <satriakrach@gmail.com>
2023-12-14 06:49:00 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sx-3rd-website/',
+//  base: '/sx-3rd-website/',
   plugins: [vue()],
 })
