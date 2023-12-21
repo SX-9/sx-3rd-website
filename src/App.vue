3afa67fa4997d179f69bde242dfc2ecfaabb1f52
Satr14 <satriakrach@gmail.com>
2023-12-21 05:23:59 +0000

    save gaza!

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@
 -->
 
 <script setup>
-import Intro from "./components/Intro.vue"
-import Repos from "./components/Repos.vue"
+import Intro from "./components/Intro.vue";
+import Repos from "./components/Repos.vue";
 import Status from "./components/Status.vue";
 import Skills from "./components/Skills.vue";
+import Gaza from "./components/Gaza.vue";
 </script>
 
 <script>
@@ -35,12 +36,13 @@ export default {
 </script>
 
 <template>
+  <Gaza />
   <div class="flex justify-center items-center flex-col p-12 pb-8">
     <h1 class="text-cyan-100 text-xl text-center">
       Welcome to my little corner of the internet!
     </h1>
-    <div class="flex flex-col sm:flex-row-reverse sm:gap-8 justify-center items-center">
-      <p class="text-gray-400 text-center m-2">Its <span class="font-bold">{{ new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }).split(' ').filter(i=>!i.includes('/')).join(' ') }}</span> in Asia/Jakarta.</p>
+    <div class="flex flex-col sm:flex-row-reverse sm:gap-8 justify-center items-center m-2">
+      <p class="text-gray-400 text-center">Its <span class="font-bold">{{ new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }).split(' ').filter(i=>!i.includes('/')).join(' ') }}</span> in Asia/Jakarta.</p>
       <div class="flex gap-4">
         <a target="_blank" href="https://github.com/SX-9/sx-3rd-website"><i class="fa-solid fa-code"></i></a>
         <a target="_blank" href="mailto:hello@mail.sx9.is-a.dev"><i class="fa-solid fa-envelope"></i></a>
---
+++ src/components/Gaza.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Free Palestine 🇵🇸",
+      description: "Stand for humanity! Palestine has a right to defend itself. Together, we can put an end to the Gaza genocide. Let's unite to #SAVEGAZA.",
+      linkTo: "https://donate.unrwa.org/",
+    }
+  }
+}
+</script>
+
+<template>
+  <div
+      class="top-0 isolate z-50 flex items-center overflow-hidden px-3.5 py-2.5 backdrop-blur-sm  md:px-6 bg-gray-600/70"
+    >
+      <div
+        class="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 blur-2xl"
+        aria-hidden="true"
+      >
+      <div
+        class="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#149954] to-[#E4312b] opacity-80 md:opacity-50"
+        :style="{
+          clipPath:'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
+        }"
+      />
+      </div>
+      <div
+        class="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 blur-2xl"
+        aria-hidden="true"
+      >
+      <div
+        class="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#149954] to-[#E4312b] opacity-80 md:opacity-50"
+        :style="{
+          clipPath:'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)'
+        }"
+      />
+      </div>
+      <p
+        class="mx-auto text-sm leading-4 sm:leading-6 text-gray-300 text-center"
+      >
+        <a :href="linkTo" target="_blank" class="nostyle">
+          <strong className="font-semibold">{{title}}</strong>
+          <br>
+          {{description}}
+        </a>
+      </p>
+    </div>
+</template>
+
