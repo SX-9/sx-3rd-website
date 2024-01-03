956b5bfa2f1f865eed3b0a11ebeed53d27dae158
SX-9 <satriakrach@gmail.com>
2024-01-03 19:24:32 +0700

    links card

---
+++ src/components/Links.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: 'Pinned Repos API',
+          url: 'https://gh-get-pinned.cyclic.app/',
+        },
+        {
+          name: 'Terminal Portfolio',
+          url: 'https://github.com/SX-9/term-port',
+        },
+        {
+          name: 'Web.SH',
+          url: 'https://sx-9.github.io/web.sh-client/',
+        },
+        {
+          name: 'Collatz Conjecture',
+          url: 'https://sx-9.github.io/collatz-conjecture/',
+        },
+        {
+          name: 'DVD Logo Bounce',
+          url: 'https://sx-9.github.io/dvd-bounce/',
+        },
+        
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card b4">
+    <h1><i class="fa-solid fa-link"></i> Links</h1>
+    <div class="p-2 gap-2">
+      <ul>
+        <li v-for="link in links">
+          <a :href="link.url" target="_blank">{{ link.name }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
