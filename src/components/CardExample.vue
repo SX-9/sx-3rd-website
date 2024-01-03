2131de9796fbc33adf06c6b8747470acfb7ccc12
SX-9 <satriakrach@gmail.com>
2024-01-03 08:14:29 +0700

    example for later

---
+++ src/components/CardExample.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    
+  }
+}
+</script>
+
+<template>
+  <div class="card b4">
+    <h1><i class="fa-solid fa-feather-pointed"></i> Miscellaneous</h1>
+    <div class="p-2 gap-2">
+
+    </div>
+  </div>
+</template>
