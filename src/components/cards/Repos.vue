5338726e8f5b820add72206112d3319fb119504e
Satr14 <satriakrach@gmail.com>
2024-06-12 11:49:31 +0000

    show lang if no stars

--- src/components/cards/Repos.vue
+++ src/components/cards/Repos.vue
@@ -13,7 +13,8 @@ async function getGitHubRepositories(username, limit) {
       name: username + '/' + repo.name,
       description: repo.description,
       link: repo.html_url,
-      stars: repo.stargazers_count
+      stars: repo.stargazers_count,
+      language: repo.language,
     }));
 
     repositoriesInfo.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
@@ -33,7 +34,8 @@ export default {
                     name: "Loading...",
                     description: "Fetching data...",
                     link: "javascript:void(0)",
-                    stars: Infinity
+                    stars: 0,
+                    language: "JavaScript",
                 }
             ]
         };
@@ -51,7 +53,8 @@ export default {
                 <hr size="1" class="pb-2 border-subtext0" v-if="i">
                 <div class="flex flex-row justify-between items-center">
                     <h2 class="text-xl"><a target="_blank" :href="repo.link">{{ repo.name }}</a></h2>
-                    <p class="text-yellow"><i class="fa-solid fa-star"></i> {{ repo.stars }}</p>
+                    <p class="text-yellow" v-if="repo.stars"><i class="fa-solid fa-star"></i> {{ repo.stars }}</p>
+                    <p class="text-subtext0 italic" v-else>{{ repo.language }}</p>
                 </div>
                 <p class="text-subtext1" v-if="repo.description">{{ repo.description }}</p>
                 <p class="text-subtext0 italic" v-else>No Description</p>
